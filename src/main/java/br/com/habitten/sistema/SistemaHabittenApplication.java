ccaaba4f4ef540e523a53af9f19b0446de26e237
lmmendonca <lmaximino.mendonca@gmail.com>
2018-11-27 15:26:44 -0200

    commit inicial

---
+++ src/main/java/br/com/habitten/sistema/SistemaHabittenApplication.java
@@ -0,0 +1,12 @@
+package br.com.habitten.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHabittenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaHabittenApplication.class, args);
+    }
+}
