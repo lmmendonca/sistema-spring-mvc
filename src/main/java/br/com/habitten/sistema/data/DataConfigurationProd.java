de1a026739fcf432baaeb4e939b3f2c5ac9d375c
lmmendonca <lmaximino.mendonca@gmail.com>
2018-11-28 16:02:55 -0200

    atualizacoes para o repositorio reroku

--- src/main/java/br/com/habitten/sistema/data/DataConfigurationProd.java
+++ src/main/java/br/com/habitten/sistema/data/DataConfigurationProd.java
@@ -1,9 +1,32 @@
 package br.com.habitten.sistema.data;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @Profile("prod")
 public class DataConfigurationProd {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl =
+                "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+                        + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
