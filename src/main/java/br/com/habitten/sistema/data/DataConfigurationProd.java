5a65f7b43ff0dce2c2b639b2bb60f8278184cc9d
lmmendonca <lmaximino.mendonca@gmail.com>
2018-11-28 16:28:03 -0200

    atualizacoes para o repositorio reroku

--- src/main/java/br/com/habitten/sistema/data/DataConfigurationProd.java
+++ src/main/java/br/com/habitten/sistema/data/DataConfigurationProd.java
@@ -12,6 +12,7 @@ import java.net.URISyntaxException;
 @Profile("prod")
 public class DataConfigurationProd {
 
+
     @Bean
     public BasicDataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
